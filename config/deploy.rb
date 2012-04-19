ddf8d07018b053e15c88976c3112fa9c0f5af623
Alexey Noskov <alexey.noskov@gmail.com>
2012-04-19 22:57:11 +0400

    Initial stub

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+set :application, "personal"
+set :repository,  "git://github.com/alno/site-personal-osm.git"
+
+set :user, "personal"
+set :use_sudo, false
+set :deploy_to, "/home/personal/apps/osm"
+
+set :scm, :git
+
+role :web, "alno.name"
+role :app, "alno.name"
+role :db,  "alno.name", :primary => true
+
+require 'bundler/capistrano'
+
+after "deploy:update_code", roles => :app do
+  run "ln -s #{shared_path}/config/unicorn.rb #{release_path}/config/unicorn.rb"
+end
+
+namespace :deploy do
+
+  desc "Restarting unicorn"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} ; ([ -f tmp/pids/unicorn.pid ] && kill -USR2 `cat tmp/pids/unicorn.pid`) || bundle exec unicorn -c config/unicorn.rb -E production -D"
+  end
+
+  desc "Rude restart application"
+  task :rude_restart, :roles => :app do
+    run "cd #{current_path} ; pkill unicorn; sleep 0.5; pkill -9 unicorn; sleep 0.5 ; bundle exec unicorn_rails -c config/unicorn.rb -E production -D "
+  end
+
+  task :start, :roles => :app do
+    rude_restart
+  end
+
+end
