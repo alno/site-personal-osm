1a5da10f8c610072ca00fc874ed42d2841411067
Alexey Noskov <alexey.noskov@gmail.com>
2012-04-21 15:27:15 +0400

    Fix in database.rb

--- lib/database.rb
+++ lib/database.rb
@@ -2,7 +2,7 @@ require 'sequel'
 
 DB_CONFIG_FILE = File.open(File.expand_path('../config/database.yml', File.dirname(__FILE__)))
 DB_CONFIG = YAML.load(DB_CONFIG_FILE)[ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development']
-DB_CONN_STRING = "postgres://#{DB_CONFIG['username']}:#{DB_CONFIG['password']}@#{DB_CONFIG['host']}"
+DB_CONN_STRING = "postgres://#{DB_CONFIG['username']}:#{DB_CONFIG['password']}@#{DB_CONFIG['host']}/#{DB_CONFIG['database']}"
 
 DB = Sequel.connect DB_CONN_STRING
 
